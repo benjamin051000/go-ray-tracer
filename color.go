d38abc797d562abb6c65a6f952668578ecc12006
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-04-18 20:53:23 -0400

    9.5: Using Gamma Correction for Accurate Color Intensity
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

--- color.go
+++ color.go
@@ -3,15 +3,25 @@ package main
 import (
 	"image"
 	"image/color"
+	"math"
 )
 
 type Color = Vec3
 
+func ToGamma(linear float64) float64 {
+	if linear > 0 {
+		return math.Sqrt(linear)
+	}
+
+	return 0
+}
+
 // TODO what kind of synchronization might this require?
 func (c Color) Write(x, y uint, img *image.RGBA) {
 	intensity := interval{0.000, 0.999}
-	r := uint8(256 * intensity.Clamp(c.x))
-	g := uint8(256 * intensity.Clamp(c.y))
-	b := uint8(256 * intensity.Clamp(c.z))
+	r := uint8(256 * intensity.Clamp(ToGamma(c.x)))
+	g := uint8(256 * intensity.Clamp(ToGamma(c.y)))
+	b := uint8(256 * intensity.Clamp(ToGamma(c.z)))
 	img.Set(int(x), int(y), color.RGBA{r, g, b, 255})
 }
+
